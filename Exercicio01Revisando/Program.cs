e17ecbb6620c790d3e8bca41cf7628a57e1eaae7
Tony <antony.branquinho@gmail.com>
2025-04-25 18:54:38 -0300

    Comentei o codigo

--- Exercicio01Revisando/Program.cs
+++ Exercicio01Revisando/Program.cs
@@ -3,7 +3,12 @@ using System;
 using System.Globalization;
 
 namespace MyApp
-{
+{ 
+    // Calcula 3 propriedades geometricas de um retangulo que é definido pelo usuario.
+    // 1. Área
+    // 2. Perímetro
+    // 3. Diagonal
+    // O retângulo é definido pelo usuário através de sua largura e altura.
     internal class Program
     {
         static void Main(string[] args)
